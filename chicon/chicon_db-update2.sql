c39a3c3e52787fd471cc7002cbce7c5f9b45c850
Vincent ANTOINE <contact.vantoine@gmail.com>
2016-03-07 16:45:41 +0100

    Small update on service_list table structure
    exec_script move to varchar(60)

--- chicon/chicon_db-update2.sql
+++ chicon/chicon_db-update2.sql
@@ -165,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `service_list` (
   `common_name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `icon` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `exec_script` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `exec_script` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `config_script` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `exec_freq` int(11) NOT NULL,
   PRIMARY KEY (`srvGlobalId`)
